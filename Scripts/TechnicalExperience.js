d0714f9279dddcec70459ade20b14bf812a5331b
elagmae <lea.pouthier@gmail.com>
2025-12-18 16:35:22 +0100

    infos_v3

--- Scripts/TechnicalExperience.js
+++ Scripts/TechnicalExperience.js
@@ -4,16 +4,15 @@ class TechnicalExperience extends HTMLElement
     {
         this.innerHTML = 
         `
-            <br>
+            <p class="Exp"> C# </p>
             <p class="Exp"> Unity </p>
             <p class="Exp"> FMOD </p>
             <p class="Exp"> Git </p>
             <p class="Exp"> Unreal </p>
-            <p class="Exp"> Illustrator </p>
-            <p class="Exp"> C# </p>
+            <p class="Exp"> Blueprint </p>
             <p class="Exp"> HTML </p>
             <p class="Exp"> CSS </p>
-            <p class="Exp"> Blueprint </p>
+            <p class="Exp"> Illustrator </p>
         `;
     }
 }
